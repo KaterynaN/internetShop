4cb97fe226b27b1ec3e529c5cc3ff0555b84da0f
Kateryna_N <kateryna.od.ua@gmail.com>
2017-10-20 11:57:05 +0300

    Edit Cart, add View for Cart

---
+++ InternetShop/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using InternetShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternetShop.Controllers
+{
+    public class CartController : Controller
+    {
+        IItemRepository rep;
+
+        public CartController(IItemRepository r)
+        {
+            rep = r;
+        }
+
+        public ActionResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel {
+                Cart = this.Cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        // GET: Cart
+        public ActionResult AddToCart(int id, string returnUrl)
+        {
+            item item = rep.GetItem(id);
+            if (item != null)
+            {
+                Cart.AddItem(item, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public ActionResult RemoveFromCart(int id, string returnUrl)
+        {
+            item item = rep.GetItem(id);
+            if (item != null)
+            {
+                Cart.RemoveItem(item);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart Cart
+        {
+            get
+            {
+                Cart cart = (Cart)Session["Cart"];
+                if (cart == null)
+                {
+                    cart = new Cart();
+                    Session["Cart"] = cart;
+                }
+                return cart;
+            }
+        }
+    }
+}
---
+++ InternetShop/Models/CartIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InternetShop.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
